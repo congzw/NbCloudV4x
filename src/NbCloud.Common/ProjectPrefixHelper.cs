dc8f905b8c577518a76ba6f0ca1caedb9f518867
congzw <46074987@qq.com>
2017-10-30 17:18:51 +0800

    add MyProjectHelper

---
+++ src/NbCloud.Common/ProjectPrefixHelper.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+
+namespace NbCloud.Common
+{
+    /// <summary>
+    /// 项目帮助类
+    /// </summary>
+    public interface IMyProjectHelper
+    {
+        /// <summary>
+        /// 获取项目前缀，例如NbLight
+        /// </summary>
+        /// <returns></returns>
+        string GetProjectPrefix();
+    }
+    
+    /// <summary>
+    /// 项目帮助类
+    /// </summary>
+    public class MyProjectHelper : IMyProjectHelper, IResolveAsSingleton
+    {
+        #region ioc helpers
+
+        /// <summary>
+        /// 支持动态替换（ResolveAsSingleton）
+        /// </summary>
+        /// <returns></returns>
+        public static IMyProjectHelper Resolve()
+        {
+            return ResolveAsSingleton<MyProjectHelper, IMyProjectHelper>.Resolve();
+        }
+
+        #endregion
+
+        private string _prefix = null;
+        public string GetProjectPrefix()
+        {
+            if (_prefix != null)
+            {
+                return _prefix;
+            }
+
+            var prefix = "NbCloud";
+            var ns = this.GetType().Namespace;
+            if (ns != null)
+            {
+                var result = ns.Split('.').FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(result))
+                {
+                    prefix = result;
+                }
+            }
+            _prefix = prefix;
+            return _prefix;
+        }
+    }
+}
