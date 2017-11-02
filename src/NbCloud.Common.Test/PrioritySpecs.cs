6eb39265c6e439abc6e915874e84ed1fb29688df
congzw <46074987@qq.com>
2017-11-02 15:22:02 +0800

    add Priority

---
+++ src/NbCloud.Common.Test/PrioritySpecs.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NbCloud.TestLib;
+
+namespace NbCloud.Common
+{
+    [TestClass]
+    public class PrioritySpecs
+    {
+        [TestMethod]
+        public void SortByPriority_Should_Ok()
+        {
+            var tasks = new List<IDemoPriority>
+            {
+                new DemoPriority(),
+                new Demo1Priority(),
+                new Demo2Priority(),
+                new Demo3Priority(),
+                new Demo4Priority()
+            };
+
+            var orderedTasks = tasks.SortByPriority().ToList();
+            orderedTasks[0].GetType().Name.ShouldEqual(typeof(Demo1Priority).Name);
+            orderedTasks[1].GetType().Name.ShouldEqual(typeof(Demo2Priority).Name);
+            orderedTasks[2].GetType().Name.ShouldEqual(typeof(DemoPriority).Name);
+            orderedTasks[3].GetType().Name.ShouldEqual(typeof(Demo3Priority).Name);
+            orderedTasks[4].GetType().Name.ShouldEqual(typeof(Demo4Priority).Name);
+        }
+    }
+
+    #region mocks
+
+    public interface IDemoPriority : IPriority
+    {
+
+    }
+    public class DemoPriority : IDemoPriority
+    {
+        public int Priority()
+        {
+            return this.Priority_Default_0();
+        }
+    }
+    public class Demo1Priority : IDemoPriority
+    {
+        public int Priority()
+        {
+            return this.Priority_Minus_10000();
+        }
+    }
+    public class Demo2Priority : IDemoPriority
+    {
+        public int Priority()
+        {
+            return this.Priority_Minus_10000() + 1;
+        }
+    }
+    public class Demo3Priority : IDemoPriority
+    {
+        public int Priority()
+        {
+            return this.Priority_Plus_10000();
+        }
+    }
+    public class Demo4Priority : IDemoPriority
+    {
+        public int Priority()
+        {
+            return this.Priority_Plus_10000() + 1;
+        }
+    }
+
+    #endregion
+}
---
+++ src/NbCloud.Common/Priority.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NbCloud.Common
+{
+    public interface IPriority
+    {
+        int Priority();
+    }
+
+    public static class PriorityExtensions
+    {
+        public static int Priority_Minus_10000(this IPriority priority)
+        {
+            return -10000;
+        }
+        public static int Priority_Default_0(this IPriority priority)
+        {
+            return 0;
+        }
+        public static int Priority_Plus_10000(this IPriority priority)
+        {
+            return 10000;
+        }
+
+        public static IOrderedEnumerable<IPriority> SortByPriority(this IEnumerable<IPriority> items)
+        {
+            var orderedItems = items.OrderBy(x => x.Priority());
+            return orderedItems;
+        }
+    }
+}
