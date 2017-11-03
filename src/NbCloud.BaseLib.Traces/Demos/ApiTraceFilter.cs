59158d386d526b200cb3b66baf6584e227f0854a
congzw <46074987@qq.com>
2017-11-03 17:07:29 +0800

    add demo trace filter for mvc and api

---
+++ src/NbCloud.BaseLib.Traces/Demos/ApiTraceFilter.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Globalization;
+//using System.Net.Http;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using System.Web.Http.Controllers;
+//using System.Web.Mvc;
+//using NbCloud.Common;
+//using Ninject.Modules;
+//using Ninject.Web.WebApi.FilterBindingSyntax;
+
+//namespace NbCloud.BaseLib.Traces.Demos
+//{
+//    public class ApiTraceFilter : System.Web.Http.Filters.IActionFilter
+//    {
+//        public bool AllowMultiple
+//        {
+//            get { return true; }
+//        }
+//        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+//        {
+//            var message = string.Format(CultureInfo.InvariantCulture, "[ApiTraceFilter Executing] {0}.{1}",
+//                actionContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+//                actionContext.ActionDescriptor.ActionName);
+//            UtilsLogger.LogMessage(message);
+            
+//            var result = await continuation();
+
+//            var message2 = string.Format(CultureInfo.InvariantCulture, "[ApiTraceFilter Executed] {0}.{1}",
+//                actionContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+//                actionContext.ActionDescriptor.ActionName);
+//            UtilsLogger.LogMessage(message2);
+//            return result;
+//        }
+//    }
+
+//    public class ApiTraceFilterModule : NinjectModule
+//    {
+//        public override void Load()
+//        {
+//            this.BindHttpFilter<ApiTraceFilter>(System.Web.Http.Filters.FilterScope.Global);
+//        }
+//    }
+//}
---
+++ src/NbCloud.BaseLib.Traces/Demos/MvcTraceFilter.cs
@@ -0,0 +1,36 @@
+﻿//using System.Globalization;
+//using System.Web.Mvc;
+//using NbCloud.Common;
+//using Ninject.Web.Mvc.FilterBindingSyntax;
+
+//namespace NbCloud.BaseLib.Traces.Demos
+//{
+//    public class MvcTraceFilter : System.Web.Mvc.IActionFilter
+//    {
+//        public void OnActionExecuting(ActionExecutingContext filterContext)
+//        {
+//            var message = string.Format(CultureInfo.InvariantCulture, "[RequestTraceFilter OnActionExecuting] {0}.{1}",
+//                filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+//                filterContext.ActionDescriptor.ActionName);
+
+//            UtilsLogger.LogMessage(message);
+//        }
+
+//        public void OnActionExecuted(ActionExecutedContext filterContext)
+//        {
+//            var message = string.Format(CultureInfo.InvariantCulture, "[RequestTraceFilter OnActionExecuted] {0}.{1}",
+//                filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+//                filterContext.ActionDescriptor.ActionName);
+
+//            UtilsLogger.LogMessage(message);
+//        }
+//    }
+
+//    public class RequestTraceBindModule : NinjectModule
+//    {
+//        public override void Load()
+//        {
+//            this.BindFilter<MvcTraceFilter>(FilterScope.Action, 0);
+//        }
+//    }
+//}
