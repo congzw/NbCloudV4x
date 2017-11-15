d6989a6ae06de3243ed41ae1abc110dcacecddd2
congzw <46074987@qq.com>
2017-11-15 18:45:27 +0800

    提交遗漏文件

---
+++ src/NbCloud.BaseLib.Traces/RunningRecords/RunningRecordTask.cs
@@ -0,0 +1,26 @@
+﻿using NbCloud.Common;
+using NbCloud.Common.Ioc;
+using NbCloud.Common.Tasks;
+
+namespace NbCloud.BaseLib.Traces.RunningRecords
+{
+    public class RunningRecordTask : IApplicationPostStartTask, IApplicationStopTask
+    {
+        public int Priority()
+        {
+            return this.Priority_Default_0();
+        }
+
+        void IApplicationPostStartTask.Execute()
+        {
+            var runningRecordService = CoreServiceProvider.LocateService<IRunningRecordService>();
+            runningRecordService.Start();
+        }
+
+        void IApplicationStopTask.Execute()
+        {
+            var runningRecordService = CoreServiceProvider.LocateService<IRunningRecordService>();
+            runningRecordService.Stop();
+        }
+    }
+}
