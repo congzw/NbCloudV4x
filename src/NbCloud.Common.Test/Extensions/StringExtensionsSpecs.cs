43b9f0fa35875d7e2eba5efe0ee1d7dcf37b133e
congzw <46074987@qq.com>
2017-11-29 16:14:04 +0800

    add string NbEquals and specs

--- src/NbCloud.Common.Test/Extensions/StringExtensionsSpecs.cs
+++ src/NbCloud.Common.Test/Extensions/StringExtensionsSpecs.cs
@@ -36,5 +36,24 @@ namespace NbCloud.Common.Extensions
             new string[] { "1", "2", "3" }.JoinToString("-").ShouldEqual("1-2-3");
             new int[] { 1, 2, 3 }.JoinToString("-").ShouldEqual("1-2-3");
         }
+
+        [TestMethod]
+        public void NbEquals_Null_Or_Empty_Should_Equal()
+        {
+            "".NbEquals(" ").ShouldTrue();
+            " ".NbEquals("").ShouldTrue();
+            "".NbEquals(null).ShouldTrue();
+            string nullValue = null;
+            nullValue.NbEquals(null).ShouldTrue();
+            nullValue.NbEquals("").ShouldTrue();
+            nullValue.NbEquals(" ").ShouldTrue();
+        }
+        [TestMethod]
+        public void NbEquals_AutoTrim_Should_Equal()
+        {
+            "A".NbEquals("a").ShouldTrue();
+            "A".NbEquals("a ").ShouldTrue();
+            "A".NbEquals(" a ").ShouldTrue();
+        }
     }
 }
--- src/NbCloud.Common/Extensions/StringExtensions.cs
+++ src/NbCloud.Common/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace NbCloud.Common.Extensions
 {
@@ -11,6 +12,23 @@ namespace NbCloud.Common.Extensions
                 return string.Empty;
             }
             return string.Join(separator, values);
+        }
+        
+        /// <summary>
+        /// 宽松的字符串比较，如果双方为空或null都视为相等
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="value2"></param>
+        /// <param name="stringComparison"></param>
+        /// <returns></returns>
+        public static bool NbEquals(this string value, string value2, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return string.IsNullOrWhiteSpace(value2);
+            }
+
+            return value.Trim().Equals(value2.Trim(), stringComparison);
         }
     }
 }
