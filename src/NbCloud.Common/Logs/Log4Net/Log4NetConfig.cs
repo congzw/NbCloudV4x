c52f392b23a67c792407770d33bb97de0ee9d577
congzw <46074987@qq.com>
2017-12-18 15:28:20 +0800

    add Log4NetConfig and setup for log4net by default

---
+++ src/NbCloud.Common/Logs/Log4Net/Log4NetConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NbCloud.Common.Logs.Log4Net
+{
+    public static class Log4NetConfig
+    {
+        public static readonly Lazy<ILoggerManager> LoggerManagerLazy = new Lazy<ILoggerManager>(() => new Log4NetLoggerManager());
+        private static Func<ILoggerManager> _loggerManagerLazyBackup = null;
+        
+        public static void Enabled()
+        {
+            if (_loggerManagerLazyBackup == null)
+            {
+                _loggerManagerLazyBackup = LoggerManager.Resolve;
+            }
+            LoggerManager.Resolve = () => LoggerManagerLazy.Value;
+        }
+        public static void Disabled()
+        {
+            if (_loggerManagerLazyBackup != null)
+            {
+                _loggerManagerLazyBackup = LoggerManager.Resolve;
+            }
+        }
+    }
+}
