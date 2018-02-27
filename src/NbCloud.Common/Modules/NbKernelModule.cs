650a7f02bbb2f386a17a88dbae166cda55593fc0
congzw <46074987@qq.com>
2018-02-27 11:39:58 +0800

    增加NbKernelModule

--- src/NbCloud.Common/Modules/NbKernelModule.cs
+++ src/NbCloud.Common/Modules/NbKernelModule.cs
@@ -1,6 +1,17 @@
 ﻿namespace NbCloud.Common.Modules
 {
-    public class NbKernelModule : NbModule
+    /// <summary>
+    /// 标识是核心模块
+    /// </summary>
+    public interface INbKernelModule
+    {
+        
+    }
+
+    /// <summary>
+    /// 标识是核心模块
+    /// </summary>
+    public abstract class NbKernelModule : NbModule, INbKernelModule
     {
     }
 }
